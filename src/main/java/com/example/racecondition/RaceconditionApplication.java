38a5b92f1c0bfccfd8127a5714a371e94087a944
choi <younggeunn@naver.com>
2026-01-08 21:32:50 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/racecondition/RaceconditionApplication.java
@@ -0,0 +1,13 @@
+package com.example.racecondition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceconditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceconditionApplication.class, args);
+    }
+
+}

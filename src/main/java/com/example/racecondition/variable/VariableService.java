1a2420a6854c8f037edd805c1ddf2db87ae803db
choi <younggeunn@naver.com>
2026-01-09 21:14:49 +0900

    [섹션 3] 5. 공유 변수에 사용하는 synchronized, ReentrantLock

--- src/main/java/com/example/racecondition/variable/VariableService.java
+++ src/main/java/com/example/racecondition/variable/VariableService.java
@@ -3,18 +3,54 @@ package com.example.racecondition.variable;
 import lombok.Getter;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 @Getter
 @Service
 public class VariableService {
 
+    // 3.4. 코드
+//    private Long stock = 0L;
+//
+//    public void resetStock() {
+//        stock = 0L;
+//    }
+//
+//    public Long increaseStock() {
+//        stock = stock + 1;
+//        return stock;
+//    }
+
+
+
+    // 3.5. 코드 (synchronized 사용버전)
+//    private Long stock = 0L;
+//
+//    public void resetStock() {
+//        stock = 0L;
+//    }
+//
+//    public synchronized Long increaseStock() {
+//        stock = stock + 1;
+//        return stock;
+//    }
+
+
+
+    // 3.5. 코드 (ReentrantLock 사용버전)
     private Long stock = 0L;
+
+    private final ReentrantLock lock = new ReentrantLock();
 
     public void resetStock() {
         stock = 0L;
     }
 
     public Long increaseStock() {
+        lock.lock();
         stock = stock + 1;
+        lock.unlock();
+
         return stock;
     }
 
